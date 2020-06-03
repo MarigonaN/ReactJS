2c865f65bd6de571d49c0453e670469ed084ead6
MarigonaN <marigonanuhija@gmail.com>
2020-06-03 23:27:50 +0200

    Update Footer.jsx

--- m4-firs-react-app/src/components/Footer.jsx
+++ m4-firs-react-app/src/components/Footer.jsx
@@ -1,49 +1,41 @@
 import React, { Component } from "react";
 import Container from "react-bootstrap/Container";
-import { Col, Image, Row } from "react-bootstrap";
+
+import { Col, Row, i} from "react-bootstrap";
 
 
 
 class Footer extends Component {
   render() {
     return (
-      <Container className="footer" fluid style={{ backgroundColor: "#92afde" }}>
+      <Container className="footer" fluid style={{ backgroundColor: "white", height: "250px", }}>
         <Container>
-          <Row className="row-cols-1 row-cols-lg-2 my-4 justify-content-start">
+          <Row className="row-cols-1 row-cols-lg-2 my-4 ">
           
-            <Col>
-              <ul>
-                <li className="pb-2">
-                  <a href="#">About</a>
-                </li>
-                <li className="pb-2">
-                  <a href="#">Contact</a>
-                </li>
-                <li className="pb-2">
-                  <a href="#">Terms of use</a>
-                </li>
-                <li className="pb-2">
-                  <a href="#">Bookshop for authors</a>
-                </li>
-                <li className="pb-2">
-                  <a className="pr-2" href="#">
-                   
-                  </a>
-                  <a className="pr-2" href={"#"}>
-                   
-                  </a>
-                  <a className="pr-2" href={"#"}>
-                   
-                  </a>
-                </li>
-              </ul>
+            <Col className="justify-content-start">
+              <img src="https://i.pinimg.com/564x/18/18/88/18188820e5da19a3908aeb18af5246b5.jpg"></img>
+          
+            </Col>
+            <Col className="justify-content-start mt-5">
+            <h3>Book Store</h3>
+            <p>Street: 120 Wentworth St. Hattiesburg, MS 39401</p>
+            <p>Tel: 202-555-0161</p>
+          <a href="#" >
+           <i className="fa fa-facebook fa-2x" ></i>
+           <i className="fa fa-github fa-2x ml-5"></i>
+           <i className="fa fa-linkedin fa-2x ml-5"></i>
+           <i className="fa fa-heart ml-5"></i>
+          </a>
+         
+         
+      
+           
             </Col>
            
+            
+           
           </Row>
-          <Col className="d-flex logo align-items-center justify-content-center">
-              <Image src="https://www.pngfind.com/pngs/m/130-1300269_books-on-shelf-png-books-on-shelf-clipart.png" />
-              
-            </Col>
+         
         </Container>
       </Container>
     );
