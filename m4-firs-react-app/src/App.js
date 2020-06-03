07aa206882881a1f47b2527cb1a7589fde8cc4dd
MarigonaN <marigonanuhija@gmail.com>
2020-06-03 16:56:28 +0200

    Create App.js

---
+++ m4-firs-react-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NavBar from "./components/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Footer from "./components/Footer";
+import JumbotronC from "./components/JumbotronC";
+import LatestRelease from "./components/LatestRelease";
+
+
+function App() {
+  return (
+    <>
+      <NavBar title={"Book Store"}/>
+      <JumbotronC title={"The best Book Shop ever! "} />
+      <LatestRelease />
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
