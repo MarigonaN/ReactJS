4966c6eb997f22e6fc9d07e9a17a003578b179f2
MarigonaN <marigonanuhija@gmail.com>
2020-06-04 00:02:08 +0200

    Update LatestRelease.jsx

--- m4-firs-react-app/src/components/LatestRelease.jsx
+++ m4-firs-react-app/src/components/LatestRelease.jsx
@@ -13,11 +13,11 @@ class LatestRelease extends Component {
         return (
            <Container>
                <h2 className="text-center">Lates Release</h2>
-               <Row className="row-cols-1 row-cols-sm-3 row-cols-md-3 row-cols-lg-5">
+               <Row className="row-cols-1 row-cols-sm-3 row-cols-md-3 row-cols-lg-5" id="books">
                    {bookList.map(book => {
                         return (
                             <Col className="mb-3">
-                                <Image src={book.img} thumbnail  style={{height:"18rem", objectFit: "cover"}}/>
+                                <Image src={book.img} thumbnail style={{height:"18rem", objectFit: "cover",}}/>
                                 <div className="w-100 d-flex justify-content-center" >
                                     <span style={{color: "#9e866b"}} className="text-center">{book.title}</span>
                                 </div>
