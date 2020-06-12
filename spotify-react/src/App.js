bb712bc1695c06da3ea172a5de393db9f8488500
MarigonaN <marigonanuhija@gmail.com>
2020-06-12 14:46:00 +0200

    Update App.js

--- spotify-react/src/App.js
+++ spotify-react/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
-import NavBar from './components/NavBar';
+import NavBar from './components/HomePage';
 import './App.css';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <HomePage/>
     </div>
      
   );
