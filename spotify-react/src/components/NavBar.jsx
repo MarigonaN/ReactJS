da2cc75127644ec929713dfdc7a3f107c3b40e0e
MarigonaN <marigonanuhija@gmail.com>
2020-06-12 14:29:45 +0200

    Update NavBar.jsx

--- spotify-react/src/components/NavBar.jsx
+++ spotify-react/src/components/NavBar.jsx
@@ -11,6 +11,13 @@ import {Navbar} from 'react-bootstrap';
            
             alignContent: "space-between",
           };
+          const mystyle1 = {
+            backgroundColor: "black",
+            color: "whitesmoke", 
+            border: "1px solid white", 
+            fontWeight: "500",
+            
+          };
         
         return (
 <div class="conatiner-fluid home-content">
@@ -28,11 +35,60 @@ import {Navbar} from 'react-bootstrap';
                     <li className="mt-3  nav-item"><a href="/"><i className="fa fa-search mr-3"></i>Search</a></li>
                     <li className="mt-3  nav-item"><a href="/"><i className="fa fa-book  mr-3"></i>Your Library</a></li>
                     </ul>
+                    <div class="col d-flex flex-column w-100">
+                <ul class="nav flex-column justify-content-center align-items-center w-100 mt-auto pb-5">
+                  <li class="nav-item w-100 mb-3">
+                      <button type="button" class="btn btn-light  w-100 rounded-pill" style={{fontWeight:"500", marginTop: "350px"}}>SIGN UP</button>
+                  </li>
+                  <li class="nav-item w-100 mb-2">
+                     <button type="button" onclick="goToLogin()" class="btn btn-secondary w-100 rounded-pill" style={mystyle1}>LOGIN</button>
+                  </li>
+                  <li class="nav-item">
+                    <small class="mr-2">Cookie</small>|<small class="ml-2">Privacy Policy</small>
+                  </li>
+                 
+                </ul>
+         </div>
     </div>
     
     </Navbar>
 </div>
 </nav>
+<div class="col-sm-9 col-md-9 col-lg-10 pb-sm-4" >
+              <nav class="navbar navbar-expand-lg navbar-light   pt-3 mb-3">
+                 
+                  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                
+                 <div class="d-flex align-items-center m-auto">
+                      <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                        <ul class="navbar-nav m-auto ">
+                          <li class="nav-item active mr-3">
+                            <a class="nav-link bolder" href="#">Trending <span class="sr-only">(current)</span></a>
+                          </li>
+                          <li class="nav-item mr-3">
+                            <a class="nav-link bolder" href="#">Podcast</a>
+                          </li>
+                         
+                          <li class="nav-item mr-3">
+                            <a class="nav-link bolder" href="#" >Moods and genres</a>
+                          </li>
+                          <li class="nav-item mr-3">
+                              <a class="nav-link bolder" href="#" >New Releases</a>
+                            </li>
+                            <li class="nav-item mr-3">
+                              <a class="nav-link bolder" href="#" >Discover</a>
+                            </li>
+                        </ul>
+                       
+                      </div>
+                 </div>
+                </nav>
+                </div>
+
+
+
 </div>
 </div>
  );
